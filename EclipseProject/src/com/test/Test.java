b3992d839e734f889bbd402bbc64ded70510b88e
11Giri <108039192+11Giri@users.noreply.github.com>
2023-02-09 20:45:36 -0500

    Update Test.java
    add id and name

--- EclipseProject/src/com/test/Test.java
+++ EclipseProject/src/com/test/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int testId= 101;
+		String name="Giriraj"
+			System.out.println(testId+" "+ name);
 	}
 
 }
